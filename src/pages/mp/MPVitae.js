b8b57f5d32ec02325211c5a3e08d640afc26a71c
Ben Scheiner <ben@carevoyance.com>
2018-03-31 22:52:26 -0700

    Add mp studio screenshots

--- src/pages/mp/MPVitae.js
+++ src/pages/mp/MPVitae.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import KiraProfile from '../../images/mp/kira-left-banner.jpg';
+import RehearseScreen from '../../images/mp/mpstudio-rehearse.png'
+import PerformScreen from '../../images/mp/mpstudio-perform.png'
 
 function MPVitae(props) {
   return (
@@ -12,15 +14,17 @@ function MPVitae(props) {
         <h2 className="white">
           Lead Software Engineer
         </h2>
-        <h3 className="red">
+        <a href='http://motorizedprecision.com/' className="no-underline"><h3 className="red">
           MOTORIZED PRECISION
-        </h3>
+        </h3></a>
         <p style={{color: '#ffffff'}}>
           Lead a small team of developers working on MP Studio, a desktop application for motion control cinematography.
           Clients use MP Studio to bring our company's 6-axis robots to life. On most shoots, our robots move in
           synchronization with other devices we have integrated, including cameras, lights, focus motors, grippers, and
           turntables.
         </p>
+        <img src={RehearseScreen} className="bordered inline-img"/>
+        <img src={PerformScreen} className="bordered space-above inline-img"/>
         <p style={{color: '#ffffff'}}>
           Provide high-quality support and feature development for top of the line clients, including Apple, Microsoft,
           and the NFL.
